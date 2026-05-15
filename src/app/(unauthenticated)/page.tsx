4d34099bf31a5bf34d18473b565c59c6edfd0b40
ayeah <187853299+ayashar@users.noreply.github.com>
2026-05-15 21:59:35 +0700

    feat(landing): add kemampuan section

--- src/app/(unauthenticated)/page.tsx
+++ src/app/(unauthenticated)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/modules/landing/hero";
 import Statistics from "@/modules/landing/statistics";
 import Explore from "@/modules/landing/explore";
+import Kemampuan from "@/modules/landing/kemampuan";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Statistics />
       <Explore />
+      <Kemampuan />
     </>
   );
 }
