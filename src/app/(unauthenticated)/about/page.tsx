ca1dbf31bbd0d1b6e1eab8d9d0a00a2fc43543a5
ayeah <187853299+ayashar@users.noreply.github.com>
2026-05-16 16:09:47 +0700

    feat(about): add subject section

--- src/app/(unauthenticated)/about/page.tsx
+++ src/app/(unauthenticated)/about/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/modules/about/hero";
 import About from "@/modules/about/about";
+import Subject from "@/modules/about/subject";
 
 export default function AboutPage() {
   return (
     <>
       <Hero />
       <About />
+      <Subject />
     </>
   )
 }
